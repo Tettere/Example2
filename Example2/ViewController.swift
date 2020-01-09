475fe986233e27dbeb58258bf54b55fc624d7016
Hiroki Naito <naitouswift@gmail.com>
2020-01-10 05:57:00 +0900

    Initial Commit

---
+++ Example2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example2
+//
+//  Created by 内藤大輝 on 2020/01/10.
+//  Copyright © 2020 Hiroki Naito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
